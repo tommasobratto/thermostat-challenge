9c6c08caf0e706df8bfe5837918abbb154353720
tommasobratto <tbratto22@gmail.com>
2015-03-04 16:45:51 +0000

    added power saving buttons

--- src/Interface.js
+++ src/Interface.js
@@ -3,14 +3,34 @@
   $(document).ready(function () {
 
   $('#thermostat').text( thermostat.temperature);
+  
   $('.increase').click(function() {
-    thermostat.increaseTemperature()
-    thermostat.maximumTemperatureCheck()
+    thermostat.increaseTemperature();
+    thermostat.maximumTemperatureCheck();
   $('#thermostat').text(thermostat.temperature);
-  $('.decrease').click(function() {
-    thermostat.decreaseTemperature()
-    thermostat.minimumTemperatureCheck()
-  $('#thermostat').text(thermostat.temperature);
-    });
   });
+  
+  $('.decrease').click(function() {
+    thermostat.decreaseTemperature();
+    thermostat.minimumTemperatureCheck();
+  $('#thermostat').text(thermostat.temperature);
+  });
+
+  $('.reset').click(function () { 
+    thermostat.resetTemperature();
+    $('#thermostat').text(thermostat.temperature);
+  })
+
+  $('.powerSavingOn').click(function () { 
+    thermostat.powerSavingOn();
+    thermostat.maximumTemperatureCheck();
+    $('#thermostat').text(thermostat.temperature);
+  })
+
+  $('.powerSavingOff').click(function () {
+    thermostat.powerSavingOff();
+    thermostat.maximumTemperatureCheck();
+    $('#thermostat').text(thermostat.temperature);
+  });
+
 });
